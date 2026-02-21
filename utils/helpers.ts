4958f26d2d94c1333d641d179926986adf9c75d6
Vanshika Bhardwaj <bhardwajvanshika085@gmail.com>
2026-02-21 18:54:40 +0530

    adding url

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,3 +1,3 @@
 export const isDev = process.env.NODE_ENV === 'development';
 
-export const ORIGIN_URL = isDev ? 'http://localhost:3000' :  process.env.NEXT_PUBLIC_APP_URL!
+export const ORIGIN_URL = isDev ? 'http://localhost:3000' : 'https://www.google.com'
